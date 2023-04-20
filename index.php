64c433948888d322ae9d492388ab9b7045df97c4
Anguie02 <1487186@senati>
2023-04-20 11:33:02 -0500

    Intro a php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wonderful</title>
+</head>
+<body>
+        DESDE EL SERVIDOR
+    <h1>Desde el servidor </h1>
+    <h2><?php echo "Con PHP" ?></h2>
+</body>
+</html>
